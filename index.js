17f4a65f50e5f1e92a9f07e3e29e83adfe9bb96c
jameem <jameem@findingstorage.com>
2020-02-16 12:33:43 +0530

    creating the maze grid

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const {
     world
 } = engine
 
+const cells = 3
 const width = 600
 const height = 600
 
@@ -29,6 +30,7 @@ const render = Render.create({
 Render.run(render)
 Runner.run(Runner.create(), engine)
 
+//Define walls
 
 const walls = [
     Bodies.rectangle(width / 2, 0, width, 40, {
@@ -46,3 +48,54 @@ const walls = [
 ]
 
 World.add(world, walls)
+
+//Maze generation
+
+const shuffle = (arr) => {
+    let counter = arr.length
+    while (counter > 0) {
+        const index = Math.floor(Math.random() * counter)
+        counter--
+
+        const temp = arr[counter]
+        arr[counter] = arr[index]
+        arr[index] = temp
+    }
+
+    return arr
+}
+
+const grid = Array(cells).fill(null).map(() => Array(cells).fill(false))
+const verticalWalls = Array(cells).fill(null).map(() => Array(cells - 1).fill(false))
+const horizontalWalls = Array(cells - 1).fill(null).map(() => Array(cells).fill(false))
+
+const startRow = Math.floor(Math.random() * cells)
+const startColumn = Math.floor(Math.random() * cells)
+
+const stepThroughCell = (row, column) => {
+
+    //If I have visited cell at [row, column], then return
+    if (grid[row][column]) {
+        return
+    }
+    //Mark this cell as being visited
+    grid[row][column] = true
+    //Assemble randomly-ordered list of neighbours
+    const neighbours = shuffle([
+        [row - 1, column],
+        [row, column + 1],
+        [row + 1, column],
+        [row, column - 1]
+    ])
+    console.log(neighbours)
+    //See if that neighbour is out of bounds
+
+    //If we have visited that neighbour, continue to next neighbour
+
+    // Remove a wall from either horizontals or verticals
+
+    //Visit that next cell
+    console.log(grid)
+}
+
+stepThroughCell(startRow, startColumn)
